99ecd6297ecda0d7257083ebf7ce46319461ecae
rstott87 <rstott87@yahoo.com>
2022-08-11 13:14:42 -0400

    added cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var catalogRouter = require("./routes/index");
 var compression = require("compression");
 var helmet = require("helmet");
+var cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,7 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
