e074fedc5f86ce81478a7bef9264433016fb169b
rstott87 <rstott87@yahoo.com>
2022-08-11 16:49:57 -0400

    added timestamp

--- models/user.js
+++ models/user.js
@@ -2,15 +2,17 @@ var mongoose = require("mongoose");
 
 var Schema = mongoose.Schema;
 
-var UserSchema = new Schema({
-  
-  first_name: { type: String, required: true, maxLength: 100 },
-  last_name: { type: String, required: true, maxLength: 100 },
-  personal_email: { type: String },
-  phone_number: {type:  String},
-  local_chapter: { type: String },
-  submission_date: { type: Date}
-});
+var UserSchema = new Schema(
+  {
+    first_name: { type: String, required: true, maxLength: 100 },
+    last_name: { type: String, required: true, maxLength: 100 },
+    personal_email: { type: String },
+    phone_number: { type: String },
+    local_chapter: { type: String },
+    submission_date: { type: Date }
+  },
+  { timestamps: true }
+);
 
 UserSchema.virtual("url").get(function () {
   return "/user/" + this._id;
