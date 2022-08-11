025ede4e0455566931b99eba8fd94e62b75f1e27
rstott87 <rstott87@yahoo.com>
2022-08-11 17:36:26 -0400

    user_list controller sorts by createdAt descending

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,7 +61,7 @@ exports.user_create_post = [
 // Display list of all Users.
 exports.user_list = function (req, res, next) {
   User.find()
-    .sort([["last_name", "ascending"]])
+    .sort([["createdAt", "descending"]])
     .exec(function (err, list_users) {
       if (err) {
         return next(err);
