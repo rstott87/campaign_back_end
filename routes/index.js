30f82231ddd87b92f662ea9eb3a7154775b2e5d0
rstott87 <rstott87@yahoo.com>
2022-08-15 14:14:43 -0400

    switched CORS back to all

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require("express");
 var router = express.Router();
 var cors = require("cors");
 
-var corsOptions = {
-  origin: "https://vercel.com/rstott87/foley-campaign",
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+
 
 var user_controller = require("../controllers/userController");
 
@@ -18,7 +15,7 @@ router.get("/", function (req, res, next) {
 router.get("/user/create", user_controller.user_create_get);
 
 // POST request for creating User.
-router.post("/user/create", cors(corsOptions), user_controller.user_create_post);
+router.post("/user/create", user_controller.user_create_post);
 
 // GET request for list of all Users.
 router.get("/users", user_controller.user_list);
